75a2875ab436fa5324999ac0ea161324a574f24b
Nitin <ntyagi@processmgmt.com>
2024-09-28 12:55:20 +0530

    My First Program

---
+++ FirstProgram.java
@@ -0,0 +1,23 @@
+public class FirstProgram{
+	public static void main(String [] args){
+		System.out.println("Calculator Program");
+		int a = 12;
+		int b = 13;
+		int sum = a+b;
+		int diff = a-b;
+		int multiplication = a*b;
+		double div = a/b;
+		System.out.println("sum = "+sum);
+		System.out.println("diff = "+diff);
+		System.out.println("multiplication = "+multiplication);
+		System.out.println("div = "+div);
+	}
+}
+
+
+/* Keywords - public , class , static, void , int double
+//  Public - Access Specifier
+    private
+    default
+    protected
+static - */
